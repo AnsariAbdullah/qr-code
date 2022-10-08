df866b4e4f9a8fe23db78d244bc6a68479514f65
AnsariAbdullah <ansariabdullahar@gmail.com>
2022-10-08 18:28:46 +0530

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import QRCode from 'qrcode';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+{/* add debounce */ }
+
+const App = () => {
+	const [enteredString, setEnteredString] = useState('')
+	const [qrLink, setQrLink] = useState('')
+	const [hideQR, setHideQR] = useState(true)
+
+	const handleChange = (e) => {
+		setEnteredString(e.target.value);
+		console.log(enteredString)
+	}
+
+	const handleClick = (e) => {
+		e.preventDefault();
+		if (enteredString !== '') {
+			QRCode.toDataURL(enteredString)
+			.then(url => {
+				setQrLink(url)
+			})
+			.catch(err => {
+				alert(err)
+			})
+			setHideQR(false)
+		} else {
+			alert('enter text please')
+		}
+	}
+
+	const generateNew = (e) => {
+		e.preventDefault();
+		setHideQR(true)
+		setQrLink('');
+		setEnteredString('')
+	}
+
+	return (
+		<div className='qr-controll-wrapper'>
+			{hideQR ?
+				// input block
+				<div>
+					<input 
+						type='text'
+						onChange={e => handleChange(e)}
+					/>					
+					<button
+						onClick={e => handleClick(e)}
+					>
+						Generate QR
+					</button>
+				</div> :
+
+				// QR block
+				<div>
+					{qrLink !== '' ?
+						<img src={qrLink} /> : null
+					}
+					<button
+						className='cancel-btn'
+						onClick={e => generateNew(e)}
+					>
+						Generate another QR
+					</button>
+				</div>
+			}
+		</div>
+	);
 }
 
 export default App;
